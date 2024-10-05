469ad50d5dec06df10c01f35aa31acff738b1f70
Jovic1912 <jovic.evta@gmail.com>
2024-10-05 16:19:50 +0700

    bootcamp

---
+++ script.js
@@ -0,0 +1,16 @@
+// let tiga = "nama";
+// let box = 12;
+
+// hasil = tiga + box;
+
+// console.log(hasil);
+
+let box = 12;
+
+if (10 > 12) {
+    console.log("ini benar");
+    
+} else {
+    console.log("ini salah");
+    
+}
